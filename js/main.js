7bc29bf25fe75e6469ea7994b75e8b21a70846e9
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-05 17:30:28 -0600

    Basic sticky navbar added

--- js/main.js
+++ js/main.js
@@ -1,77 +1,17 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-//Based on the Scroller function from @sallar
-var $content = $('header .content')
-  , $blur    = $('header .overlay')
-  , wHeight  = $(window).height();
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-$(window).on('resize', function(){
-  wHeight = $(window).height();
-});
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-window.requestAnimFrame = (function()
-{
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-function Scroller()
-{
-  this.latestKnownScrollY = 0;
-  this.ticking            = false;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+} else {
+    navbar.classList.remove("sticky");
 }
-
-Scroller.prototype = {
- 
-  init: function() {
-    window.addEventListener('scroll', this.onScroll.bind(this), false);
-    $blur.css('background-image',$('header:first-of-type').css('background-image'));
-  },
-
-
-  onScroll: function() {
-    this.latestKnownScrollY = window.scrollY;
-    this.requestTick();
-  },
-
-  
-  requestTick: function() {
-    if( !this.ticking ) {
-      window.requestAnimFrame(this.update.bind(this));
-    }
-    this.ticking = true;
-  },
-
-  update: function() {
-    var currentScrollY = this.latestKnownScrollY;
-    this.ticking       = false;
-    
-    
-    var slowScroll = currentScrollY / 2
-      , blurScroll = currentScrollY * 2
-      , opaScroll = 1.4 - currentScrollY / 400;
-   if(currentScrollY > wHeight)
-     $('nav').css('position','fixed');
-   else
-     $('nav').css('position','absolute');
-    
-    $content.css({
-      'transform'         : 'translateY(' + slowScroll + 'px)',
-      '-moz-transform'    : 'translateY(' + slowScroll + 'px)',
-      '-webkit-transform' : 'translateY(' + slowScroll + 'px)',
-      'opacity' : opaScroll
-    });
-    
-    $blur.css({
-      'opacity' : blurScroll / wHeight
-    });
-  }
-};
-
-
-var scroller = new Scroller();  
-scroller.init();
-
+} 
