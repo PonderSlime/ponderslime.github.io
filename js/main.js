0eaf00f07da9d6d6560e1ee142a04a742a7e6e52
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-07 14:48:31 -0600

    Common footer partially implemented

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
-} else {
-    navbar.classList.remove("sticky");
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
-} 
